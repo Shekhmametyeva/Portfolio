b331baf9dda887ef443577f7c3800c983414d31c
Shekhmametyeva <alina-r-s98@outlook.com>
2022-07-24 17:58:49 +0400

    feat: add check on callback

--- shopping-list/my-app/src/components/buttonSvgComponents/ButtonSvgComponents.js
+++ shopping-list/my-app/src/components/buttonSvgComponents/ButtonSvgComponents.js
@@ -3,13 +3,21 @@ import './ButtonSvgComponents.css';
 import IconsSVG from '../../assets/icons/sprite.svg';
 
 class ButtonSvgComponents extends React.Component {
+
+    isCallback() {
+        if(this.props.callback) {
+            this.props.callback(this.props.value)
+        }
+        return
+    }
     
     render() {
         const isClass = this.props.class ? `button ${this.props.class}` : `button`;
+        
         return(
-            <button className={isClass} onClick={() => this.props.callback(this.props.value)}>
+            <button className={isClass} onClick={() => this.isCallback()}>
                 <svg className={`icon`}>
-                    <use xlinkHref={`${IconsSVG}#${this.props.name}` } />
+                    <use xlinkHref={`${IconsSVG}#${this.props.name}`} />
                 </svg>
             </button>
         )
