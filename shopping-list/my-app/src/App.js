18d3d14513b5d994b72921bfb5f5d2b833cf2607
Shekhmametyeva <alina-r-s98@outlook.com>
2022-07-24 12:50:02 +0400

    feat: add render wrapper

--- shopping-list/my-app/src/App.js
+++ shopping-list/my-app/src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div>Начало проекта</div>   
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className='wrapper'>
+        <div className='wrapper__ligth'>
+          <div className='shopping'>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
+
 
 export default App;
