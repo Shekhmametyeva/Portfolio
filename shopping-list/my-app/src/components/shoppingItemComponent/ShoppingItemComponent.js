6aec6465ebae3f6197fd7fed846be1e4ade7741f
Shekhmametyeva <alina-r-s98@outlook.com>
2022-07-26 11:53:42 +0400

    feat: add edit switch for items

--- shopping-list/my-app/src/components/shoppingItemComponent/ShoppingItemComponent.js
+++ shopping-list/my-app/src/components/shoppingItemComponent/ShoppingItemComponent.js
@@ -9,19 +9,18 @@ class ShoppingItemComponent extends React.Component {
         this.state ={
             isInput: false,
         }
-        this.openInput = this.openInput.bind(this)
-    }
-
-    openInput() {
-        this.setState({isInput: !this.state.isInput})
     }
     
     render() {
-        const isInput = this.state.isInput ? <InputItemComponent value ={this.props.value} element={{title: this.props.element.title, id: this.props.element.id}} callback={{openInput: this.openInput, editData: this.props.callback.editData, changeStateitemValue: this.props.callback.changeStateitemValue}}/> : null;
+        const isInput = this.props.isInput ? 
+        <InputItemComponent 
+        value ={this.props.value} 
+        element={{title: this.props.element.title, id: this.props.element.id}} 
+        callback={{openInput: () => this.props.onEditChange(''), editData: this.props.callback.editData, changeStateitemValue: this.props.callback.changeStateitemValue}}/> : null;
         return (
-            <div className="shopping__item" id={this.props.element.id} onDoubleClick={() => this.openInput()}>
+            <div className="shopping__item" id={this.props.element.id} onDoubleClick={() => this.props.onEditChange(this.props.element.id)}>
                 <p className="shopping__text">{this.props.element.title}</p>
-                <ButtonSvgComponents name={'edit'} class={'shopping__icon'} iconClass={'shopping__icon'} callback={this.openInput}/>
+                <ButtonSvgComponents name={'edit'} class={'shopping__icon'} iconClass={'shopping__icon'} callback={() => this.props.onEditChange(this.props.element.id)}/>
                 <ButtonSvgComponents name={'delete'} class={'shopping__icon'} iconClass={'shopping__icon'} callback={this.props.callback.deleteItems} value={[this.props.element]}/>             
                 {isInput}
             </div>
--- shopping-list/my-app/src/components/shoppingListComponent/ShoppingListComponent.js
+++ shopping-list/my-app/src/components/shoppingListComponent/ShoppingListComponent.js
@@ -3,7 +3,12 @@ import React from 'react';
 import ShoppingItemComponent from '../shoppingItemComponent/ShoppingItemComponent';
 
 class ShoppingListComponent extends React.Component {
-    
+    constructor(props) {
+        super(props);
+        this.state = {
+            editedItemId: ''
+        }
+    }
     render() {
         if(!this.props.data.length) {
             return(
@@ -12,7 +17,12 @@ class ShoppingListComponent extends React.Component {
         }
         return (
             <div className='shopping__list'>
-                {this.props.data.map(el => <ShoppingItemComponent element={el} key={el.id} callback={this.props.callback} value={this.props.value}/>)}
+                {this.props.data.map(el => <ShoppingItemComponent 
+                    isInput={this.state.editedItemId === el.id}
+                    onEditChange={id => this.setState({...this.state, editedItemId: id})} 
+                    element={el} key={el.id} 
+                    callback={this.props.callback} 
+                    value={this.props.value}/>)}
             </div>
         )
     }
