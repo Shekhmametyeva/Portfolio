94b252101bb58c0fc3c4b2c3799d1988b2c80ec6
Shekhmametyeva <alina-r-s98@outlook.com>
2022-07-26 11:37:25 +0400

    fix: change inputCompon for better functionality

--- shopping-list/my-app/src/components/inputComponent/InputComponent.js
+++ shopping-list/my-app/src/components/inputComponent/InputComponent.js
@@ -1,18 +1,22 @@
 import './InputComponent.css';
 import React from 'react';
 import ButtonSvgComponents from '../buttonSvgComponent/ButtonSvgComponent';
+// import { deleteAward } from '../../api/api';
 
-class InputComponent extends React.Component {
-    
+class FormComponent extends React.Component {
+    handleSubmit(event){
+        event.preventDefault();
+        this.props.callback(this.props.value)
+    }
     render() {
         return (
-            <div className="shopping__form">
+            <form className="shopping__form" onSubmit={(event) => this.handleSubmit(event)}>
                 <input type='text' placeholder='Что добавим в список' value={this.props.value} className='shopping__input' onChange={(event) => {this.props.changeStateValue(event.target.value)}}/>
-                <ButtonSvgComponents name={'add'} callback={this.props.callback} value = {this.props.value} />
-            </div>
+                <ButtonSvgComponents type='submit' name={'add'} />
+            </form>
             
         )
     }
 }
 
-export default InputComponent;
+export default FormComponent;
