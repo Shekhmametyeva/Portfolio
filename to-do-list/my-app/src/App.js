9db8208373043ea78bc2ee58c16e944cf17378b0
Shekhmametyeva <alina-r-s98@outlook.com>
2022-08-01 16:11:17 +0400

    feat: add to-do list components to App

--- to-do-list/my-app/src/App.js
+++ to-do-list/my-app/src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import FormComponent from './components/FormComponent/FormComponent';
+import ToDoListComponent from './components/ToDoListComponent/ToDoListComponent';
+import React, {useState} from 'react';
 
 
 function App() {
+  const [data, setData] = useState([]);
   return (
     <div className='wrapper'>
       <div className='todo'>
-        <FormComponent />
+        <FormComponent data={data} updateState={(value) => setData((arr) => [...arr, value])}/>
+        <ToDoListComponent data={data}/>
       </div>
     </div>
   );
