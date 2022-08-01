ca93b37954f2126b49f7c7ef6fd3bef9ac8dbc1c
Shekhmametyeva <alina-r-s98@outlook.com>
2022-08-01 16:09:00 +0400

    feat: add check validity

--- to-do-list/my-app/src/components/FormComponent/FormComponent.js
+++ to-do-list/my-app/src/components/FormComponent/FormComponent.js
@@ -2,13 +2,28 @@ import './FormComponent.css';
 import ButtonSvgComponent from '../ButtonSvgComponent/ButtonSvgComponent';
 import React, { useState } from 'react';
 
+function checkValidity (props, value) {
+    if(!value.trim()) {
+        console.log('пустой')
+        return null
+    }
+    if(props.data.find(el => el === value)) {
+        console.log('уже есть')
+        return null
+    }
+    return value
+}
 
-function FormComponent () {
+
+function FormComponent (props) {
     const [value, setValue] = useState('');
     return (
         <form className='todo__form' 
             onSubmit={(event) => {
                 event.preventDefault();
+                if(checkValidity (props, value)) {
+                    props.updateState(value)
+                }
                 setValue('')
             }}> 
             <input type='text' placeholder='Что добавим в список задач?' value={value} className='todo__input' onChange={(event) => setValue(event.target.value)}/>
