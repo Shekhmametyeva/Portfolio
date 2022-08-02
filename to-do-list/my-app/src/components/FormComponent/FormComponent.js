c436315001d36caa49a8847107f409a83c4b15f1
Shekhmametyeva <alina-r-s98@outlook.com>
2022-08-02 12:50:38 +0400

    refactor: code refactor

--- to-do-list/my-app/src/components/FormComponent/FormComponent.js
+++ to-do-list/my-app/src/components/FormComponent/FormComponent.js
@@ -26,7 +26,13 @@ function FormComponent (props) {
                 }
                 setValue('')
             }}> 
-            <input type='text' placeholder='Что добавим в список задач?' value={value} className='todo__input' onChange={(event) => setValue(event.target.value)}/>
+            <input 
+                type='text' 
+                placeholder='Что добавим в список задач?' 
+                value={value} 
+                className='todo__input' 
+                onChange={(event) => setValue(event.target.value)}
+            />
             <ButtonSvgComponent name='add' type='submit'/>
         </form>
     )
