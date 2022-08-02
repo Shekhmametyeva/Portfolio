5479acb73c7edcc0427fb9e9e84382526398ceb5
Shekhmametyeva <alina-r-s98@outlook.com>
2022-08-02 12:51:12 +0400

    feat: add task execution

--- to-do-list/my-app/src/components/ButtonSvgComponent/ButtonSvgComponent.js
+++ to-do-list/my-app/src/components/ButtonSvgComponent/ButtonSvgComponent.js
@@ -4,10 +4,18 @@ import iconsSVG from '../../assets/icons/sprite.svg';
 
 function ButtonSvgComponent (props) {
     return (
-        <button className={props.class ? `button ${props.class}` : `button`} type={props.type}>
-            <svg className={props.class ? 'todo__icon' : 'icon'}>
-                <use xlinkHref={`${iconsSVG}#${props.name}`} />
-            </svg>
+        <button 
+            className={props.class ? `button ${props.class}` : `button`} 
+            type={props.type} 
+            onClick={() => {
+                if(props.callback) {
+                    props.callback();
+                }
+                
+            }}>
+                <svg className={props.class ? 'todo__icon' : 'icon'}>
+                    <use xlinkHref={`${iconsSVG}#${props.name}`} />
+                </svg>
         </button>
     )
 }
--- to-do-list/my-app/src/components/ToDoItemComponent/ToDoItemComponent.js
+++ to-do-list/my-app/src/components/ToDoItemComponent/ToDoItemComponent.js
@@ -1,11 +1,25 @@
 import './ToDoItemComponent.css';
 import ButtonSvgComponent from '../ButtonSvgComponent/ButtonSvgComponent';
+import React, { useState } from 'react';
 
 function ToDoItemComponent (props) {
+    const [done, setDone] = useState(false);
+    if(done) {
+        return (
+            <div className={done ? 'todo__item done' : 'todo__item'} >
+                <h5 className='todo__text'>{props.element}</h5>
+            </div>
+        )
+    }
     return (
-        <div className='todo__item'>
+        <div className={done ? 'todo__item done' : 'todo__item'} >
             <h5 className='todo__text'>{props.element}</h5>
-            <ButtonSvgComponent name='done' class='todo__button'/>
+            <ButtonSvgComponent 
+                callback={() => setDone(true)}
+                name='done' 
+                class='todo__button' 
+                
+            />
         </div>
         
     )
